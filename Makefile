f83af306e0c7e1ac295f3d0d97711b8245206e6b
bishal7679 <bishalhnj127@gmail.com>
2023-05-13 23:54:49 +0530

    added script
    Signed-off-by: bishal7679 <bishalhnj127@gmail.com>

---
+++ Makefile
@@ -0,0 +1,20 @@
+GOOS_LINUX = linux
+GOOS_WINDOWS = windows
+GOOS_MACOS = darwin
+
+GOARCH_LINUX = amd64
+GOARCH_WINDOWS = amd64
+GOARCH_MACOS = arm64
+
+
+install_linux:
+	env GOOS=${GOOS_LINUX} GOARCH=${GOARCH_LINUX} ./builder.sh
+
+install_macos:
+	env GOOS=${GOOS_MACOS} GOARCH=${GOARCH_MACOS} ./builder.sh
+
+install_macos_intel:
+	env GOOS=${GOOS_MACOS} GOARCH=${GOARCH_MACOS_INTEL} ./builder.sh
+
+uninstall:
+	./uninstall.sh
--- builder.sh
+++ builder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-git clone https://github.com/bishal7679/ksapify.git
-cd ksapify/cli || echo -e "\033[31;40mPath couldn't be found\033[0m\n"
+
+cd ./cli || echo -e "\033[31;40mPath couldn't be found\033[0m\n"
 
 # go building ...
 go get -d
@@ -10,12 +10,7 @@ chmod +x ksapify
 # moving binary to local/bin to make it executable
 sudo mv -v ksapify /usr/local/bin/ksapify
 
-cd ..
-cd ..
-sudo rm -fr ksapify
-
 
 echo -e "\033[32;40mINSTALLATION COMPLETED!\033[0m\n"
-
 
 cd - || echo -e "\033[31;40mFailed to move to previous directory\033[0m\n"
