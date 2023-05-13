4269a159e41c457786657b1a9ff5610bec31a8bb
bishal7679 <bishalhnj127@gmail.com>
2023-05-13 12:18:28 +0530

    added script
    Signed-off-by: bishal7679 <bishalhnj127@gmail.com>

--- install.sh
+++ install.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-
-cd ./cli || echo -e "\033[31;40mPath couldn't be found\033[0m\n"
-
-# go building ...
-go get -d
-go build -v -o ksapify .
-chmod +x ksapify
-
-# moving binary to local/bin to make it executable
-sudo mv -v ksapify /usr/local/bin/ksapify
+sh ./builder.sh
 
 
 echo -e "\033[32;40mINSTALLATION COMPLETED!\033[0m\n"
