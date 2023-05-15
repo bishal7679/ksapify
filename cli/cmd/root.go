985f0be8542c433aabafd49e7e035ba7175d9b75
Bishal Das <70086051+bishal7679@users.noreply.github.com>
2023-05-15 19:15:20 +0530

    Update root.go

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -17,20 +17,10 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "ksapify",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Multi-Featured Light Kubernetes command-line tool",
+	Long: `A Multi-Featured Light Kubernetes command-line tool which can interact with k8s api-server and perform operations`,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := RootCmd.Execute()
 	if err != nil {
@@ -39,14 +29,6 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.ksapify.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 	RootCmd.AddCommand(get.GetobjectCmd)
 	RootCmd.AddCommand(create.CreateobjectCmd)
